bbb2f8aaace68ced79095482fedfb070bb74befb
18059104@qq.com <huijia264>
2019-09-02 10:18:14 +0800

    精简配置，只留一些简单的；

--- Smart/window-basic-setting.cpp
+++ Smart/window-basic-setting.cpp
@@ -37,61 +37,70 @@
 #include "window-basic-main.hpp"
 #include "window-basic-setting.hpp"
 #include <util/profiler.hpp>
+#include <sstream>
 
 using namespace std;
+
+static inline QString GetComboData(QComboBox *combo)
+{
+	int idx = combo->currentIndex();
+	if (idx == -1)
+		return QString();
+	return combo->itemData(idx).toString();
+}
 
 static inline bool WidgetChanged(QWidget *widget)
 {
 	return widget->property("changed").toBool();
 }
 
+/* parses "[width]x[height]", string, i.e. 1024x768 */
+static bool ConvertResText(const char *res, uint32_t &cx, uint32_t &cy)
+{
+	BaseLexer lex;
+	base_token token;
+
+	lexer_start(lex, res);
+
+	/* parse width */
+	if (!lexer_getbasetoken(lex, &token, IGNORE_WHITESPACE))
+		return false;
+	if (token.type != BASETOKEN_DIGIT)
+		return false;
+
+	cx = std::stoul(token.text.array);
+
+	/* parse 'x' */
+	if (!lexer_getbasetoken(lex, &token, IGNORE_WHITESPACE))
+		return false;
+	if (strref_cmpi(&token.text, "x") != 0)
+		return false;
+
+	/* parse height */
+	if (!lexer_getbasetoken(lex, &token, IGNORE_WHITESPACE))
+		return false;
+	if (token.type != BASETOKEN_DIGIT)
+		return false;
+
+	cy = std::stoul(token.text.array);
+
+	/* shouldn't be any more tokens after this */
+	if (lexer_getbasetoken(lex, &token, IGNORE_WHITESPACE))
+		return false;
+
+	return true;
+}
+
 /* clang-format off */
+#define CBEDIT_CHANGED  SIGNAL(editTextChanged(const QString &))
 #define COMBO_CHANGED   SIGNAL(currentIndexChanged(int))
 #define CHECK_CHANGED   SIGNAL(clicked(bool))
+#define SCROLL_CHANGED  SIGNAL(valueChanged(int))
 
 #define GENERAL_CHANGED SLOT(GeneralChanged())
-#define AUDIO_CHANGED   SLOT(AudioChanged())
 #define VIDEO_CHANGED   SLOT(VideoChanged())
-
-void OBSBasicSetting::HookWidget(QWidget *widget, const char *signal, const char *slot)
-{
-	QObject::connect(widget, signal, this, slot);
-	widget->setProperty("changed", QVariant(false));
-}
-
-#ifdef _WIN32
-void OBSBasicSetting::ToggleDisableAero(bool checked)
-{
-	SetAeroEnabled(!checked);
-}
-#endif
-
-void OBSBasicSetting::GeneralChanged()
-{
-	if (!loading) {
-		generalChanged = true;
-		sender()->setProperty("changed", QVariant(true));
-		EnableApplyButton(true);
-	}
-}
-
-void OBSBasicSetting::AudioChanged()
-{
-	if (!loading) {
-		audioChanged = true;
-		sender()->setProperty("changed", QVariant(true));
-		EnableApplyButton(true);
-	}
-}
-
-void OBSBasicSetting::VideoChanged()
-{
-	if (!loading) {
-		videoChanged = true;
-		sender()->setProperty("changed", QVariant(true));
-		EnableApplyButton(true);
-	}
-}
+#define VIDEO_RESTART   SLOT(VideoChangedRestart())
+#define VIDEO_RES       SLOT(VideoChangedResolution())
 
 OBSBasicSetting::OBSBasicSetting(QWidget *parent)
 	: QDialog(parent),
@@ -130,12 +139,20 @@ void OBSBasicSetting::OnSetupLoad()
 
 	// 由于延迟加载，需要在这里更新左侧列表栏对应图标...
 	ui->listWidget->item(0)->setIcon(generalIcon);
-	ui->listWidget->item(1)->setIcon(audioIcon);
-	ui->listWidget->item(2)->setIcon(videoIcon);
+	ui->listWidget->item(1)->setIcon(videoIcon);
 
 	/* clang-format off */
 	HookWidget(ui->language, COMBO_CHANGED, GENERAL_CHANGED);
 	HookWidget(ui->enableAutoUpdates, CHECK_CHANGED, GENERAL_CHANGED);
+
+	HookWidget(ui->baseResolution, CBEDIT_CHANGED, VIDEO_RES);
+	HookWidget(ui->outputResolution, CBEDIT_CHANGED, VIDEO_RES);
+	HookWidget(ui->downscaleFilter, COMBO_CHANGED, VIDEO_CHANGED);
+	HookWidget(ui->fpsType, COMBO_CHANGED, VIDEO_CHANGED);
+	HookWidget(ui->fpsCommon, COMBO_CHANGED, VIDEO_CHANGED);
+	HookWidget(ui->fpsInteger, SCROLL_CHANGED, VIDEO_CHANGED);
+	HookWidget(ui->fpsNumerator, SCROLL_CHANGED, VIDEO_CHANGED);
+	HookWidget(ui->fpsDenominator, SCROLL_CHANGED, VIDEO_CHANGED);
 
 #ifdef _WIN32
 	uint32_t winVer = GetWindowsVersion();
@@ -159,8 +176,6 @@ void OBSBasicSetting::LoadSettings(bool changedOnly)
 {
 	if (!changedOnly || generalChanged)
 		LoadGeneralSettings();
-	if (!changedOnly || audioChanged)
-		LoadAudioSettings();
 	if (!changedOnly || videoChanged)
 		LoadVideoSettings();
 }
@@ -179,14 +194,82 @@ void OBSBasicSetting::SaveGeneralSettings()
 	}
 }
 
-void OBSBasicSetting::SaveAudioSettings()
-{
-
-}
-
 void OBSBasicSetting::SaveVideoSettings()
 {
+	QString baseResolution = ui->baseResolution->currentText();
+	QString outputResolution = ui->outputResolution->currentText();
+	int fpsType = ui->fpsType->currentIndex();
+	uint32_t cx = 0, cy = 0;
 
+	/* ------------------- */
+
+	if (WidgetChanged(ui->baseResolution) &&
+		ConvertResText(QT_TO_UTF8(baseResolution), cx, cy)) {
+		config_set_uint(main->Config(), "Video", "BaseCX", cx);
+		config_set_uint(main->Config(), "Video", "BaseCY", cy);
+	}
+
+	if (WidgetChanged(ui->outputResolution) &&
+		ConvertResText(QT_TO_UTF8(outputResolution), cx, cy)) {
+		config_set_uint(main->Config(), "Video", "OutputCX", cx);
+		config_set_uint(main->Config(), "Video", "OutputCY", cy);
+	}
+
+	if (WidgetChanged(ui->fpsType)) {
+		config_set_uint(main->Config(), "Video", "FPSType", fpsType);
+	}
+
+	SaveCombo(ui->fpsCommon, "Video", "FPSCommon");
+	SaveSpinBox(ui->fpsInteger, "Video", "FPSInt");
+	SaveSpinBox(ui->fpsNumerator, "Video", "FPSNum");
+	SaveSpinBox(ui->fpsDenominator, "Video", "FPSDen");
+	SaveComboData(ui->downscaleFilter, "Video", "ScaleType");
+
+#ifdef _WIN32
+	if (toggleAero) {
+		SaveCheckBox(toggleAero, "Video", "DisableAero");
+		aeroWasDisabled = toggleAero->isChecked();
+	}
+#endif
+}
+
+void OBSBasicSetting::SaveCombo(QComboBox *widget, const char *section, const char *value)
+{
+	if (WidgetChanged(widget)) {
+		config_set_string(main->Config(), section, value, QT_TO_UTF8(widget->currentText()));
+	}
+}
+
+void OBSBasicSetting::SaveComboData(QComboBox *widget, const char *section, const char *value)
+{
+	if (WidgetChanged(widget)) {
+		QString str = GetComboData(widget);
+		config_set_string(main->Config(), section, value, QT_TO_UTF8(str));
+	}
+}
+
+void OBSBasicSetting::SaveCheckBox(QAbstractButton *widget,
+	const char *section, const char *value, bool invert)
+{
+	if (WidgetChanged(widget)) {
+		bool checked = widget->isChecked();
+		if (invert) checked = !checked;
+		config_set_bool(main->Config(), section, value, checked);
+	}
+}
+
+void OBSBasicSetting::SaveEdit(QLineEdit *widget, const char *section, const char *value)
+{
+	if (WidgetChanged(widget)) {
+		config_set_string(main->Config(), section, value, QT_TO_UTF8(widget->text()));
+	}
+}
+
+void OBSBasicSetting::SaveSpinBox(QSpinBox *widget, const char *section, const char *value)
+{
+	if (WidgetChanged(widget)) {
+		config_set_int(main->Config(), section, value, widget->value());
+	}
 }
 
 void OBSBasicSetting::LoadGeneralSettings()
@@ -221,65 +304,259 @@ void OBSBasicSetting::LoadLanguageList()
 	ui->language->model()->sort(0);
 }
 
-/*void OBSBasicSetting::LoadThemeList()
+static string ResString(uint32_t cx, uint32_t cy)
 {
-	// Save theme if user presses Cancel 
-	savedTheme = string(App()->GetTheme());
+	stringstream res;
+	res << cx << "x" << cy;
+	return res.str();
+}
 
-	ui->theme->clear();
-	QSet<QString> uniqueSet;
-	string themeDir;
-	char userThemeDir[512];
-	int ret = GetConfigPath(userThemeDir, sizeof(userThemeDir),	"obs-smart/themes/");
-	GetDataFilePath("themes/", themeDir);
+/* some nice default output resolution vals */
+static const double vals[] = {
+	1.0, 1.25, (1.0 / 0.75),
+	1.5, (1.0 / 0.6), 1.75, 
+	2.0, 2.25, 2.5, 2.75, 3.0
+};
 
-	// Check user dir first. 
-	if (ret > 0) {
-		QDirIterator it(QString(userThemeDir), QStringList() << "*.qss", QDir::Files);
-		while (it.hasNext()) {
-			it.next();
-			QString name = it.fileName().section(".", 0, 0);
-			ui->theme->addItem(name);
-			uniqueSet.insert(name);
+static const size_t numVals = sizeof(vals) / sizeof(double);
+
+void OBSBasicSetting::ResetDownscales(uint32_t cx, uint32_t cy)
+{
+	QString oldOutputRes;
+	string bestScale;
+	int bestPixelDiff = 0x7FFFFFFF;
+	uint32_t out_cx = outputCX;
+	uint32_t out_cy = outputCY;
+
+	//QString advRescale;
+	//QString advRecRescale;
+	//QString advFFRescale;
+	//advRescale = ui->advOutRescale->lineEdit()->text();
+	//advRecRescale = ui->advOutRecRescale->lineEdit()->text();
+	//advFFRescale = ui->advOutFFRescale->lineEdit()->text();
+
+	ui->outputResolution->blockSignals(true);
+	ui->outputResolution->clear();
+
+	//ui->advOutRescale->clear();
+	//ui->advOutRecRescale->clear();
+	//ui->advOutFFRescale->clear();
+
+	if (!out_cx || !out_cy) {
+		out_cx = cx; out_cy = cy;
+		oldOutputRes = ui->baseResolution->lineEdit()->text();
+	} else {
+		oldOutputRes = QString::number(out_cx) + "x" + QString::number(out_cy);
+	}
+
+	for (size_t idx = 0; idx < numVals; idx++) {
+		uint32_t downscaleCX = uint32_t(double(cx) / vals[idx]);
+		uint32_t downscaleCY = uint32_t(double(cy) / vals[idx]);
+		uint32_t outDownscaleCX = uint32_t(double(out_cx) / vals[idx]);
+		uint32_t outDownscaleCY = uint32_t(double(out_cy) / vals[idx]);
+
+		downscaleCX &= 0xFFFFFFFC;
+		downscaleCY &= 0xFFFFFFFE;
+		outDownscaleCX &= 0xFFFFFFFE;
+		outDownscaleCY &= 0xFFFFFFFE;
+
+		string res = ResString(downscaleCX, downscaleCY);
+		string outRes = ResString(outDownscaleCX, outDownscaleCY);
+		ui->outputResolution->addItem(res.c_str());
+		//ui->advOutRescale->addItem(outRes.c_str());
+		//ui->advOutRecRescale->addItem(outRes.c_str());
+		//ui->advOutFFRescale->addItem(outRes.c_str());
+
+		// always try to find the closest output resolution to the
+		// previously set output resolution 
+		int newPixelCount = int(downscaleCX * downscaleCY);
+		int oldPixelCount = int(out_cx * out_cy);
+		int diff = abs(newPixelCount - oldPixelCount);
+
+		if (diff < bestPixelDiff) {
+			bestScale = res;
+			bestPixelDiff = diff;
 		}
 	}
 
-	QString defaultTheme;
-	defaultTheme += DEFAULT_THEME;
-	defaultTheme += " ";
-	defaultTheme += QTStr("Default");
+	string res = ResString(cx, cy);
 
-	// Check shipped themes. 
-	QDirIterator uIt(QString(themeDir.c_str()), QStringList() << "*.qss", QDir::Files);
-	while (uIt.hasNext()) {
-		uIt.next();
-		QString name = uIt.fileName().section(".", 0, 0);
+	float baseAspect = float(cx) / float(cy);
+	float outputAspect = float(out_cx) / float(out_cy);
 
-		if (name == DEFAULT_THEME)
-			name = defaultTheme;
+	bool closeAspect = close_float(baseAspect, outputAspect, 0.01f);
+	if (closeAspect)
+		ui->outputResolution->lineEdit()->setText(oldOutputRes);
+	else
+		ui->outputResolution->lineEdit()->setText(bestScale.c_str());
 
-		if (!uniqueSet.contains(name) && name != "Default")
-			ui->theme->addItem(name);
+	ui->outputResolution->blockSignals(false);
+
+	if (!closeAspect) {
+		ui->outputResolution->setProperty("changed", QVariant(true));
+		videoChanged = true;
 	}
 
-	std::string themeName = App()->GetTheme();
+	/*if (advRescale.isEmpty())
+		advRescale = res.c_str();
+	if (advRecRescale.isEmpty())
+		advRecRescale = res.c_str();
+	if (advFFRescale.isEmpty())
+		advFFRescale = res.c_str();*/
 
-	if (themeName == DEFAULT_THEME)
-		themeName = QT_TO_UTF8(defaultTheme);
+	//ui->advOutRescale->lineEdit()->setText(advRescale);
+	//ui->advOutRecRescale->lineEdit()->setText(advRecRescale);
+	//ui->advOutFFRescale->lineEdit()->setText(advFFRescale);
+}
 
-	int idx = ui->theme->findText(themeName.c_str());
-	if (idx != -1)
-		ui->theme->setCurrentIndex(idx);
-}*/
-
-void OBSBasicSetting::LoadAudioSettings()
+void OBSBasicSetting::LoadDownscaleFilters()
 {
+	ui->downscaleFilter->addItem(
+		QTStr("Basic.Settings.Video.DownscaleFilter.Bilinear"),
+		QT_UTF8("bilinear"));
+	ui->downscaleFilter->addItem(
+		QTStr("Basic.Settings.Video.DownscaleFilter.Area"),
+		QT_UTF8("area"));
+	ui->downscaleFilter->addItem(
+		QTStr("Basic.Settings.Video.DownscaleFilter.Bicubic"),
+		QT_UTF8("bicubic"));
+	//ui->downscaleFilter->addItem(
+	//	QTStr("Basic.Settings.Video.DownscaleFilter.Lanczos"),
+	//	QT_UTF8("lanczos"));
 
+	const char *scaleType = config_get_string(main->Config(), "Video", "ScaleType");
+
+	if (astrcmpi(scaleType, "bilinear") == 0)
+		ui->downscaleFilter->setCurrentIndex(0);
+	//else if (astrcmpi(scaleType, "lanczos") == 0)
+	//	ui->downscaleFilter->setCurrentIndex(3);
+	else if (astrcmpi(scaleType, "area") == 0)
+		ui->downscaleFilter->setCurrentIndex(1);
+	else
+		ui->downscaleFilter->setCurrentIndex(2);
+}
+
+void OBSBasicSetting::LoadResolutionLists()
+{
+	uint32_t cx = config_get_uint(main->Config(), "Video", "BaseCX");
+	uint32_t cy = config_get_uint(main->Config(), "Video", "BaseCY");
+	uint32_t out_cx = config_get_uint(main->Config(), "Video", "OutputCX");
+	uint32_t out_cy = config_get_uint(main->Config(), "Video", "OutputCY");
+
+	ui->baseResolution->clear();
+
+	auto addRes = [this](int cx, int cy) {
+		QString res = ResString(cx, cy).c_str();
+		if (ui->baseResolution->findText(res) == -1)
+			ui->baseResolution->addItem(res);
+	};
+
+	for (QScreen *screen : QGuiApplication::screens()) {
+		QSize as = screen->size();
+		addRes(as.width(), as.height());
+	}
+
+	addRes(1920, 1080);
+	addRes(1280, 720);
+
+	string outputResString = ResString(out_cx, out_cy);
+
+	ui->baseResolution->lineEdit()->setText(ResString(cx, cy).c_str());
+
+	this->RecalcOutputResPixels(outputResString.c_str());
+	this->ResetDownscales(cx, cy);
+
+	ui->outputResolution->lineEdit()->setText(outputResString.c_str());
+}
+
+#define INVALID_RES_STR "Basic.Settings.Video.InvalidResolution"
+
+static bool ValidResolutions(Ui::OBSBasicSetting *ui)
+{
+	QString baseRes = ui->baseResolution->lineEdit()->text();
+	QString outputRes = ui->outputResolution->lineEdit()->text();
+	uint32_t cx, cy;
+
+	if (!ConvertResText(QT_TO_UTF8(baseRes), cx, cy) ||
+		!ConvertResText(QT_TO_UTF8(outputRes), cx, cy)) {
+		ui->videoMsg->setText(QTStr(INVALID_RES_STR));
+		return false;
+	}
+
+	ui->videoMsg->setText("");
+	return true;
+}
+
+void OBSBasicSetting::RecalcOutputResPixels(const char *resText)
+{
+	uint32_t newCX;
+	uint32_t newCY;
+
+	ConvertResText(resText, newCX, newCY);
+	if (newCX && newCY) {
+		outputCX = newCX;
+		outputCY = newCY;
+	}
+}
+static inline void LoadFPSCommon(OBSBasic *main, Ui::OBSBasicSetting *ui)
+{
+	const char *val = config_get_string(main->Config(), "Video", "FPSCommon");
+
+	int idx = ui->fpsCommon->findText(val);
+	if (idx == -1) idx = 4;
+	ui->fpsCommon->setCurrentIndex(idx);
+}
+
+static inline void LoadFPSInteger(OBSBasic *main, Ui::OBSBasicSetting *ui)
+{
+	uint32_t val = config_get_uint(main->Config(), "Video", "FPSInt");
+	ui->fpsInteger->setValue(val);
+}
+
+static inline void LoadFPSFraction(OBSBasic *main, Ui::OBSBasicSetting *ui)
+{
+	uint32_t num = config_get_uint(main->Config(), "Video", "FPSNum");
+	uint32_t den = config_get_uint(main->Config(), "Video", "FPSDen");
+
+	ui->fpsNumerator->setValue(num);
+	ui->fpsDenominator->setValue(den);
+}
+
+void OBSBasicSetting::LoadFPSData()
+{
+	LoadFPSCommon(main, ui.get());
+	LoadFPSInteger(main, ui.get());
+	LoadFPSFraction(main, ui.get());
+
+	uint32_t fpsType = config_get_uint(main->Config(), "Video", "FPSType");
+	if (fpsType > 2) fpsType = 0;
+
+	ui->fpsType->setCurrentIndex(fpsType);
+	ui->fpsTypes->setCurrentIndex(fpsType);
 }
 
 void OBSBasicSetting::LoadVideoSettings()
 {
+	loading = true;
 
+	if (obs_video_active()) {
+		ui->videoPage->setEnabled(false);
+		ui->videoMsg->setText(QTStr("Basic.Settings.Video.CurrentlyActive"));
+	}
+
+	this->LoadResolutionLists();
+	this->LoadFPSData();
+	this->LoadDownscaleFilters();
+
+#ifdef _WIN32
+	if (toggleAero) {
+		bool disableAero = config_get_bool(main->Config(), "Video", "DisableAero");
+		toggleAero->setChecked(disableAero);
+		aeroWasDisabled = disableAero;
+	}
+#endif
+
+	loading = false;
 }
 
 void OBSBasicSetting::on_listWidget_itemSelectionChanged()
@@ -334,8 +611,6 @@ void OBSBasicSetting::SaveSettings()
 {
 	if (generalChanged)
 		SaveGeneralSettings();
-	if (audioChanged)
-		SaveAudioSettings();
 	if (videoChanged)
 		SaveVideoSettings();
 	if (videoChanged)
@@ -349,8 +624,6 @@ void OBSBasicSetting::SaveSettings()
 		std::string changed;
 		if (generalChanged)
 			AddChangedVal(changed, "general");
-		if (audioChanged)
-			AddChangedVal(changed, "audio");
 		if (videoChanged)
 			AddChangedVal(changed, "video");
 		blog(LOG_INFO, "Settings changed (%s)", changed.c_str());
@@ -381,4 +654,72 @@ bool OBSBasicSetting::QueryChanges()
 
 	ClearChanged();
 	return true;
+}
+
+void OBSBasicSetting::on_outputResolution_editTextChanged(const QString &text)
+{
+	if (!loading) {
+		this->RecalcOutputResPixels(QT_TO_UTF8(text));
+	}
+}
+
+void OBSBasicSetting::on_baseResolution_editTextChanged(const QString &text)
+{
+	if (!loading && ValidResolutions(ui.get())) {
+		QString baseResolution = text;
+		uint32_t cx, cy;
+
+		ConvertResText(QT_TO_UTF8(baseResolution), cx, cy);
+		this->ResetDownscales(cx, cy);
+	}
+}
+
+void OBSBasicSetting::HookWidget(QWidget *widget, const char *signal, const char *slot)
+{
+	QObject::connect(widget, signal, this, slot);
+	widget->setProperty("changed", QVariant(false));
+}
+
+#ifdef _WIN32
+void OBSBasicSetting::ToggleDisableAero(bool checked)
+{
+	SetAeroEnabled(!checked);
+}
+#endif
+
+void OBSBasicSetting::GeneralChanged()
+{
+	if (!loading) {
+		generalChanged = true;
+		sender()->setProperty("changed", QVariant(true));
+		EnableApplyButton(true);
+	}
+}
+
+void OBSBasicSetting::VideoChanged()
+{
+	if (!loading) {
+		videoChanged = true;
+		sender()->setProperty("changed", QVariant(true));
+		EnableApplyButton(true);
+	}
+}
+
+void OBSBasicSetting::VideoChangedResolution()
+{
+	if (!loading && ValidResolutions(ui.get())) {
+		videoChanged = true;
+		sender()->setProperty("changed", QVariant(true));
+		EnableApplyButton(true);
+	}
+}
+
+void OBSBasicSetting::VideoChangedRestart()
+{
+	if (!loading) {
+		videoChanged = true;
+		ui->videoMsg->setText(QTStr("Basic.Settings.ProgramRestart"));
+		sender()->setProperty("changed", QVariant(true));
+		EnableApplyButton(true);
+	}
 }
--- Smart/window-basic-setting.hpp
+++ Smart/window-basic-setting.hpp
@@ -39,27 +39,33 @@ class OBSBasicSetting : public QDialog {
 	Q_OBJECT
 	// 需要修改 UI/data/themes 下面的 .qss 文件，只能分开写样式...
 	Q_PROPERTY(QIcon generalIcon READ GetGeneralIcon WRITE SetGeneralIcon DESIGNABLE true)
-	Q_PROPERTY(QIcon audioIcon READ GetAudioIcon WRITE SetAudioIcon DESIGNABLE true)
 	Q_PROPERTY(QIcon videoIcon READ GetVideoIcon WRITE SetVideoIcon DESIGNABLE true)
 private:
 	OBSBasic * main;
 	std::unique_ptr<Ui::OBSBasicSetting> ui;
 
 	bool generalChanged = false;
-	bool audioChanged = false;
 	bool videoChanged = false;
 	int  pageIndex = 0;
 	bool loading = true;
 
+	uint32_t outputCX = 0;
+	uint32_t outputCY = 0;
+
+	void SaveCombo(QComboBox *widget, const char *section, const char *value);
+	void SaveEdit(QLineEdit *widget, const char *section, const char *value);
+	void SaveSpinBox(QSpinBox *widget, const char *section, const char *value);
+	void SaveComboData(QComboBox *widget, const char *section, const char *value);
+	void SaveCheckBox(QAbstractButton *widget, const char *section, const char *value, bool invert = false);
+
 	inline bool Changed() const {
-		return generalChanged || audioChanged || videoChanged;
+		return generalChanged || videoChanged;
 	}
 	inline void EnableApplyButton(bool en) {
 		ui->buttonBox->button(QDialogButtonBox::Apply)->setEnabled(en);
 	}
 	inline void ClearChanged() {
 		generalChanged = false;
-		audioChanged = false;
 		videoChanged = false;
 		EnableApplyButton(false);
 	}
@@ -75,33 +81,41 @@ private:
 
 	void LoadSettings(bool changedOnly);
 	void LoadGeneralSettings();
-	void LoadAudioSettings();
 	void LoadVideoSettings();
 
 	/* general */
 	void LoadLanguageList();
+
+	/* video */
+	void LoadRendererList();
+	void LoadDownscaleFilters();
+	void LoadResolutionLists();
+	void LoadFPSData();
+
+	void ResetDownscales(uint32_t cx, uint32_t cy);
+	void RecalcOutputResPixels(const char *resText);
+
 private:
 	void SaveGeneralSettings();
-	void SaveAudioSettings();
 	void SaveVideoSettings();
 	void SaveSettings();
 
 	QIcon generalIcon;
-	QIcon audioIcon;
 	QIcon videoIcon;
 
 	QIcon GetGeneralIcon() const { return generalIcon; }
-	QIcon GetAudioIcon() const { return audioIcon; }
 	QIcon GetVideoIcon() const { return videoIcon; }
 private slots:
     void OnSetupLoad();
 	void GeneralChanged();
-	void AudioChanged();
 	void VideoChanged();
+	void VideoChangedRestart();
+	void VideoChangedResolution();
 	void on_listWidget_itemSelectionChanged();
+	void on_outputResolution_editTextChanged(const QString &text);
+	void on_baseResolution_editTextChanged(const QString &text);
 	void on_buttonBox_clicked(QAbstractButton *button);
 	void SetGeneralIcon(const QIcon &icon) { generalIcon = icon; }
-	void SetAudioIcon(const QIcon &icon) { audioIcon = icon; }
 	void SetVideoIcon(const QIcon &icon) { videoIcon = icon; }
 protected:
 	virtual void closeEvent(QCloseEvent *event);
